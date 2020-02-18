a48f5779322aae10b0011c7eb6c095a6de1b4bf7
michaeld55 <michaeld55@hotmail.co.uk>
2020-02-18 17:06:36 +0000

    added months checks

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -21,12 +21,52 @@ def multiply( num1, num2)
   return result
 end
 
-def divide( num1, num2)
+def divide( num1, num2 )
   result = num1.to_i() / num2.to_i()
   return result
 end
 
-def length_of_string(input)
+def length_of_string( input )
   result = input.length
   return result
+end
+
+def join_string( string_1, string_2 )
+
+  joined_string = string_1 + string_2
+  return joined_string
+
+end
+
+def add_string_as_number( num1, num2 )
+  result = num1.to_i() + num2.to_i()
+  return result
+end
+
+def number_to_full_month_name( month_number )
+
+  months = {"January" => 1, "Febuary" => 2, "March" => 3, "April" =>4, "May" => 5,
+  "June" => 6,"July" => 7, "August" => 8, "September" => 9, "October" => 10,
+  "November" => 11, "December" => 12}
+
+  months.each() do| name, number|
+
+      if month_number == number
+        return name
+      end
+  end
+end
+
+def number_to_short_month_name( month_number )
+
+  months = {"Jan" => 1, "Feb" => 2, "Mar" => 3, "Apr" =>4, "May" => 5,
+  "Jun" => 6,"Jul" => 7, "Aug" => 8, "Sep" => 9, "Oct" => 10,
+  "Nov" => 11, "Dec" => 12}
+  months.each() do| name, number|
+
+      if month_number == number
+        return name
+      end
+  end
+
 end
